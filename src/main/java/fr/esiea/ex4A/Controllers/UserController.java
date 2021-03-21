1faa593aeef4c4a76647f2b6a6cd52cc7c423692
VCamozzi <valentin.camozzi@icloud.com>
2021-03-21 17:39:34 +0100

    part 2.2 => api match ok

--- src/main/java/fr/esiea/ex4A/Controllers/UserController.java
+++ src/main/java/fr/esiea/ex4A/Controllers/UserController.java
@@ -1,10 +1,16 @@
 package fr.esiea.ex4A.Controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.esiea.ex4A.Match;
 import fr.esiea.ex4A.Storage.UserRepository;
 import fr.esiea.ex4A.User;
     import org.springframework.http.MediaType;
     import org.springframework.web.bind.annotation.*;
-    import java.util.Map;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 class UserController {
@@ -25,4 +31,21 @@ class UserController {
         }
         return user;
     }
+
+    @GetMapping(path="/api/matches", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    String matchSearch(@RequestParam(name="userName", required=true) String userName, @RequestParam(name="userCountry", required=true) String userCountry)  throws IOException{
+        ObjectMapper mapper = new ObjectMapper();
+        Match match = new Match("toto", "totodu26");
+        Match match2 = new Match("tata", "tatadu77");
+
+        List<Match> matchList = new ArrayList<>();
+        matchList.add(match);
+        matchList.add(match2);
+        String result = mapper.writeValueAsString(matchList);
+
+        return result;
+    }
+
+
 }
