5241c5336560d9f4b9b625860338927d5e62981f
VCamozzi <valentin.camozzi@icloud.com>
2021-03-22 23:00:27 +0100

    package Storage ok

---
+++ src/test/java/fr/esiea/ex4A/Services/AgifyServicesTest.java
@@ -0,0 +1,16 @@
+package fr.esiea.ex4A.Services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AgifyServicesTest {
+
+    @Test
+    void getAgeFromNameAndCountryTest() {
+    }
+
+    @Test
+    void getMatchFromAge() {
+    }
+}
