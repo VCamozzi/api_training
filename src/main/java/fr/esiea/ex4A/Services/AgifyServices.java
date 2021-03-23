8e1e5d4a347f20128e3f9a1807d00bbe5f471324
VCamozzi <valentin.camozzi@icloud.com>
2021-03-23 11:08:25 +0100

    mise en cache ok

--- src/main/java/fr/esiea/ex4A/Services/AgifyServices.java
+++ src/main/java/fr/esiea/ex4A/Services/AgifyServices.java
@@ -9,11 +9,14 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class AgifyServices {
     private final AgifyClient client;
     private final UserRepository repository;
+    private final Map<String,Integer> cache = new HashMap<>();
 
     public AgifyServices(AgifyClient client, UserRepository repository) {
         this.client = client;
@@ -29,7 +32,7 @@ public class AgifyServices {
         ArrayList<Match> matchList = new ArrayList<Match>();
         for (User match : repository.getUsers()){
             if (match.getuName() != null) {
-                int matchAge = getAgeFromNameAndCountry(match.getuName(), match.getuCountry()).getAge();
+                int matchAge = IsInCache(match);
                 int ageDiff = (userAge - matchAge);
                 if (ageDiff <= 4 && ageDiff >= -4) {
                     Match newMatch = new Match(match.getuName(), match.getuTweeter());
@@ -38,5 +41,19 @@ public class AgifyServices {
             }
         }
         return matchList;
+    }
+
+    public int IsInCache(User user) throws IOException {
+        int value;
+        if (cache.containsKey(user.getuName() + user.getuCountry())){
+            value = cache.get(user.getuName() + user.getuCountry());
+            System.out.println("récupération du chache : " + value + " pour : " + user.getuName());
+        }else{
+            Agify agifyUser = getAgeFromNameAndCountry(user.getuName(), user.getuCountry());
+            cache.put(user.getuName() + user.getuCountry(), agifyUser.getAge());
+            value = agifyUser.getAge();
+            System.out.println("mise en cache : " + user.getuName() + " - " + user.getuCountry() + " - " + value);
+        }
+        return value;
     }
 }
